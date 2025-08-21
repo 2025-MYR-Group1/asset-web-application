fe19aeaf738f6369fa027f03b6b02f6d5f7d5ab2
2hnK <kimgh921@gmail.com>
2025-08-21 11:30:19 +0800

    feat(schemas): modify schemas for asset, movement, response

---
+++ app/services/schemas/asset.py
@@ -0,0 +1,83 @@
+from datetime import date, datetime
+from decimal import Decimal
+
+from sqlmodel import Field, SQLModel
+
+
+class AssetCreate(SQLModel):
+    category: str = Field(max_length=255)
+    type: str = Field(max_length=255)
+    brand: str = Field(max_length=255)
+    name: str = Field(max_length=255)
+    supplier: str = Field(max_length=255)
+
+    serial_no: str = Field(max_length=255)
+    invoice_no: str = Field(max_length=255)
+
+    purchase_date: date
+    warranty_start_date: date
+    warranty_end_date: date
+
+    location: str = Field(max_length=255)
+    assigned_campus: str = Field(max_length=255)
+    assigned_department: str = Field(max_length=255)
+
+    manager_name: str = Field(max_length=255)
+    status: str = Field(max_length=255)
+    depreciation: Decimal = Field(default=Decimal("0.00"))
+
+
+class AssetRead(SQLModel):
+    id: int
+
+    category: str
+    type: str
+    brand: str
+    name: str
+    supplier: str
+
+    serial_no: str
+    invoice_no: str
+
+    purchase_date: date
+    warranty_start_date: date
+    warranty_end_date: date
+
+    location: str
+    assigned_campus: str
+    assigned_department: str
+
+    manager_name: str
+    status: str
+    depreciation: Decimal
+
+    created_at: datetime | None
+    updated_at: datetime | None
+
+
+class AssetUpdate(SQLModel):
+    category: str | None = None
+    type: str | None = None
+    brand: str | None = None
+    name: str | None = None
+    supplier: str | None = None
+
+    serial_no: str | None = None
+    invoice_no: str | None = None
+
+    purchase_date: date | None = None
+    warranty_start_date: date | None = None
+    warranty_end_date: date | None = None
+
+    location: str | None = None
+    assigned_campus: str | None = None
+    assigned_department: str | None = None
+
+    manager_name: str | None = None
+    status: str | None = None
+    depreciation: Decimal | None = None
+
+
+class AssetDelete(SQLModel):
+    id: int
+    deleted: bool = False
